d14a096e416b91fd0358a686ea2b446230958f5b
SylvainMacabrey <smacabre@univ-fcomte.fr>
2018-03-02 13:54:33 +0100

    un truc

--- src/ThreadClient.java
+++ src/ThreadClient.java
@@ -18,26 +18,45 @@ class ThreadClient extends Thread  {
 		int turn = 0;
 		try {
 		    while (!stop) {
-				/* A COMPLETER :
-				   - lire une ligne dans le fichier xbee
-				   - analyzer la trame
-				   - si position actuelle = celle du barycentre, arrêter la voiture
-				   - si turn vaut 0, envoyer requete SETPOSITION et récupérer le barycentre courant
-				*/
+				// lire une ligne dans le fichier xbee
 				line = comm.readFromXbee();
+				// analyzer la trame
 				analyzer.analyzeFrame(line);
-				currentCenter = new Position(analyzer.getLatitude(), analyzer.getLongitude(), analyzer.getAltitude());
-
+				// si position actuelle = celle du barycentre, arrêter la voiture
+				Position posActuelle = new Position(analyzer.getLatitude(), analyzer.getLongitude(), analyzer.getAltitude());
+				if(currentCenter == posActuelle) {
+					byte[] cmdCar = new byte[16];
+					cmdCar[0] = 1;
+					cmdCar[1] = 1;
+					cmdCar[2] = 0;
+					cmdCar[3] = 1;
+					cmdCar[4] = 0;
+					cmdCar[5] = 0;
+					cmdCar[6] = 0;
+					cmdCar[7] = 0;
+					cmdCar[8] = 0;
+					cmdCar[9] = 1;
+					cmdCar[10] = 0;
+					cmdCar[11] = 0;
+					cmdCar[12] = 0;
+					cmdCar[13] = 0;
+					cmdCar[14] = 0;
+					cmdCar[15] = 0;
+					comm.writeToXbee(cmdCar);
+				}
+				// si turn vaut 0, envoyer requete SETPOSITION et récupérer le barycentre courant
 				turn  = (turn+1)%4;
 				if(turn == 0) {
-					Position barycentre = comm.sendSetPositionRequest();
+					currentCenter = comm.sendSetPositionRequest(analyzer.getLatitude(), analyzer.getLongitude(), analyzer.getAltitude());
 				}
 
 		    }
 		}
 		catch(IOException e) {
 		    System.out.println("cannot communicated with server. Aborting");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
 		}
-    }
+	}
 }
 		
